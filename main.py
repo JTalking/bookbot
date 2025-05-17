95d8ec92cc871472dd413fbfa0383753182e10e5
JTalking <josiahfabbott@gmail.com>
2025-05-17 02:15:08 -0400

    C: created dictionary, about to sort

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ def get_book_text(path_to_file):
         return file_contents
 
 from stats import get_num_words
+from stats import character_count
 
 def main():
     path = "./books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print (f"{num_words} words found in the document")
+    print (character_count(text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    lower_text = text.lower()
+    char_dict = {}
+    for char in lower_text:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    return char_dict
