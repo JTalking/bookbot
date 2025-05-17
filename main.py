efc25e7adc8f942601d2e217de22ddd27d4ea97e
JTalking <josiahfabbott@gmail.com>
2025-05-17 01:39:31 -0400

    B: Just split the functions, about to count characters

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+from stats import get_num_words
+
+def main():
+    path = "./books/frankenstein.txt"
+    text = get_book_text(path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+main()
